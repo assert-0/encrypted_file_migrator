f48b1a3a60c44cee8cda6bc348ef6a478a0451be
Nikola Socec <nikola.socec@gmail.com>
2025-02-20 15:47:37 +0100

    Implemented archive metadata and conflict analysis.

--- encrypted_file_migrator/models/args.py
+++ encrypted_file_migrator/models/args.py
@@ -1,7 +1,8 @@
-from .consts import OperationType
+from ..consts import OperationType
+from .model import Model
 
 
-class Args:
+class Args(Model):
     def __init__(
             self,
             operation: OperationType,
@@ -9,7 +10,8 @@ class Args:
             threads: int,
             manifest_path: str,
             exclude_manifest_path: str,
-            destination_path: str, source_backup_path: str
+            destination_path: str, source_backup_path: str,
+            metadata_path: str,
     ):
         self.operation = operation
         self.encryption_password = encryption_password
@@ -18,20 +20,10 @@ class Args:
         self.exclude_manifest_path = exclude_manifest_path
         self.destination_path = destination_path
         self.source_backup_path = source_backup_path
+        self.metadata_path = metadata_path
 
         self.validate()
 
     def validate(self):
         if self.operation not in OperationType.__members__.values():
             raise ValueError(f"Unrecognized operation: '{self.operation}'")
-
-    def __str__(self):
-        return str({
-            "operation": self.operation,
-            "encryption_password": self.encryption_password,
-            "threads": self.threads,
-            "manifest_path": self.manifest_path,
-            "exclude_manifest_path": self.exclude_manifest_path,
-            "destination_path": self.destination_path,
-            "source_backup_path": self.source_backup_path,
-        })
--- encrypted_file_migrator/operations.py
+++ encrypted_file_migrator/operations.py
@@ -1,3 +1,4 @@
+import json
 import multiprocessing
 import os
 import subprocess
@@ -5,8 +6,11 @@ from abc import ABC, abstractmethod
 from getpass import getpass
 from typing import Type, List, Dict
 
-from .args import Args
-from .consts import OperationType
+from .consts import OperationType, METADATA_SUFFIX, MIGRATION_SUFFIX, \
+    ANALYSIS_FILE
+from .models.analysis import Analysis
+from .models.args import Args
+from .models.metadata import Metadata
 from .utils import to_engineering_notation
 
 
@@ -87,43 +91,6 @@ class OperationsFactory:
 
 
 class Backup(Operation):
-    def _tar_command(self) -> List[str]:
-        command = [
-            f"tar",
-            f"--create",
-            f"--verbose",
-            f"--acls",
-            f"--selinux",
-            f"--xattrs",
-            f"--absolute-names",
-            (
-                f"--exclude-from={self.args.exclude_manifest_path}"
-                if self.args.exclude_manifest_path else ""
-            ),
-            f"--files-from={self.args.manifest_path}",
-        ]
-
-        return command
-
-    def _zstd_command(self) -> List[str]:
-        return [
-            f"zstd",
-            f"--compress",
-            f"--threads={self.args.threads}",
-            f"--stdout",
-        ]
-
-    def _openssl_command(self) -> List[str]:
-        return [
-            f"openssl",
-            f"enc",
-            f"-e",
-            f"-aes-256-cbc",
-            f"-pbkdf2",
-            f"-k", f"{self.args.encryption_password}",
-            f"-out", f"{self.args.destination_path}",
-        ]
-
     def validate(self) -> None:
         super().validate()
 
@@ -165,6 +132,11 @@ class Backup(Operation):
         if self.args.encryption_password != confirm_password:
             raise ValueError("Encryption passwords do not match")
 
+        if not self.args.metadata_path:
+            self.args.metadata_path = (
+                    self.args.destination_path + METADATA_SUFFIX
+            )
+
     def execute(self) -> None:
         print("Calculating total size of files to backup...")
         with open(self.args.manifest_path, "r") as f:
@@ -192,49 +164,78 @@ class Backup(Operation):
                 self._tar_command(),
                 self._pv_command(int(size)),
                 self._zstd_command(),
-                self._openssl_command(),
+                self._openssl_command(self.args.destination_path),
             ]
         )
         pipeline[-1].communicate()
 
         print("Backup completed")
 
+        print("Saving metadata...")
 
-class Restore(Operation):
+        with open(self.args.exclude_manifest_path, "r") as f:
+            exclude_manifest_patterns = [
+                line.strip() for line in f.readlines()
+            ]
+
+        metadata = Metadata(
+            input_manifest_files=files,
+            exclude_manifest_patterns=exclude_manifest_patterns,
+            total_size=int(size),
+        )
+
+        metadata_json = json.dumps(metadata.model_dump())
+
+        openssl = subprocess.Popen(
+            self._openssl_command(self.args.metadata_path),
+            stdin=subprocess.PIPE,
+        )
+        zstd = subprocess.Popen(
+            self._zstd_command(),
+            stdin=subprocess.PIPE,
+            stdout=openssl.stdin,
+        )
+        zstd.communicate(input=metadata_json.encode())
+
     def _tar_command(self) -> List[str]:
-        return [
+        command = [
             f"tar",
-            f"--extract",
+            f"--create",
             f"--verbose",
             f"--acls",
             f"--selinux",
             f"--xattrs",
             f"--absolute-names",
-            f"--same-permissions",
-            f"--same-owner",
-            f"--backup",
-            f"--suffix=.migration.bak",
+            (
+                f"--exclude-from={self.args.exclude_manifest_path}"
+                if self.args.exclude_manifest_path else ""
+            ),
+            f"--files-from={self.args.manifest_path}",
         ]
+
+        return command
 
     def _zstd_command(self) -> List[str]:
         return [
             f"zstd",
-            f"--decompress",
+            f"--compress",
             f"--threads={self.args.threads}",
             f"--stdout",
         ]
 
-    def _openssl_command(self) -> List[str]:
+    def _openssl_command(self, destination_path: str) -> List[str]:
         return [
             f"openssl",
             f"enc",
-            f"-d",
+            f"-e",
             f"-aes-256-cbc",
             f"-pbkdf2",
             f"-k", f"{self.args.encryption_password}",
-            f"-in", f"{self.args.source_backup_path}",
+            f"-out", f"{destination_path}",
         ]
 
+
+class Restore(Operation):
     def validate(self) -> None:
         super().validate()
 
@@ -253,16 +254,32 @@ class Restore(Operation):
         if not self.args.encryption_password:
             raise ValueError("Encryption password is required")
 
-    def execute(self) -> None:
-        files = [self.args.source_backup_path]
-        sizes = self._execute_command(self._du_command(files)).split("\n")
+        if not self.args.metadata_path:
+            self.args.metadata_path = (
+                self.args.source_backup_path + METADATA_SUFFIX
+            )
 
-        # Extract the total size from last non-blank line
-        size = float(sizes[-2].split()[0])
+        if not os.path.exists(self.args.metadata_path):
+            raise ValueError(
+                f"Metadata path '{self.args.metadata_path}' does not exist"
+            )
+
+    def execute(self) -> None:
+        print("Reading metadata...")
+
+        pipeline = self._create_pipeline(
+            [
+                self._openssl_command(self.args.metadata_path),
+                self._zstd_command(),
+            ]
+        )
+        metadata_json = pipeline[-1].communicate()[0].decode("utf-8")
+        metadata_dict = json.loads(metadata_json)
+        metadata = Metadata.model_load(metadata_dict)
 
         print(
-            f"Size of encrypted backup: "
-            f"{to_engineering_notation(size)} bytes"
+            f"Size of backup: "
+            f"{to_engineering_notation(metadata.total_size)} bytes"
         )
 
         continue_flag = input("Do you want to continue? [Y/n]: ")
@@ -274,15 +291,71 @@ class Restore(Operation):
 
         pipeline = self._create_pipeline(
             [
-                self._openssl_command(),
-                self._pv_command(int(size)),
+                self._openssl_command(self.args.source_backup_path),
                 self._zstd_command(),
+                self._pv_command(metadata.total_size),
                 self._tar_command(),
             ]
         )
         pipeline[-1].communicate()
 
         print("Restore completed")
+
+        print("Analyzing restore conflicts (old files backed up)...")
+
+        conflicting_files = []
+        for file in metadata.input_manifest_files:
+            if os.path.isfile(file):
+                if os.path.exists(file + MIGRATION_SUFFIX):
+                    conflicting_files.append(file)
+            elif os.path.isdir(file):
+                for root, _, files in os.walk(file):
+                    for f in files:
+                        f = os.path.join(root, f)
+                        if os.path.exists(f + MIGRATION_SUFFIX):
+                            conflicting_files.append(f)
+
+        analysis = Analysis(conflicting_files=conflicting_files)
+        analysis_json = json.dumps(analysis.model_dump())
+        with open(ANALYSIS_FILE, "w") as f:
+            f.write(analysis_json)
+
+        print("Analysis completed")
+        print(f"Analysis file saved to '{os.getcwd()}/{ANALYSIS_FILE}'")
+
+    def _tar_command(self) -> List[str]:
+        return [
+            f"tar",
+            f"--extract",
+            f"--verbose",
+            f"--acls",
+            f"--selinux",
+            f"--xattrs",
+            f"--absolute-names",
+            f"--same-permissions",
+            f"--same-owner",
+            f"--backup",
+            f"--suffix={MIGRATION_SUFFIX}",
+        ]
+
+    def _zstd_command(self) -> List[str]:
+        return [
+            f"zstd",
+            f"--decompress",
+            f"--threads={self.args.threads}",
+            f"--stdout",
+        ]
+
+    def _openssl_command(self, source_path: str) -> List[str]:
+        return [
+            f"openssl",
+            f"enc",
+            f"-d",
+            f"-aes-256-cbc",
+            f"-pbkdf2",
+            f"-k", f"{self.args.encryption_password}",
+            f"-in", f"{source_path}",
+        ]
 
 
 OperationsFactory.register_operation(OperationType.BACKUP, Backup)
